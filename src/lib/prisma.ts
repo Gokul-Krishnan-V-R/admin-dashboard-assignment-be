deb8fac1a553683405ab26610c18cbcda975d680
Gokul-Krishnan-V-R <143732362+Gokul-Krishnan-V-R@users.noreply.github.com>
2025-03-14 18:44:18 +0530

    Add files via upload

---
+++ src/lib/prisma.ts
@@ -0,0 +1,15 @@
+// import { PrismaClient } from '@prisma/client';
+
+// declare global {
+//     // eslint-disable-next-line no-var
+//     var prisma: PrismaClient | undefined
+// }
+
+// export const client = globalThis.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
+
+import { PrismaClient } from '@prisma/client'
+
+const client = new PrismaClient()
+export { client }
